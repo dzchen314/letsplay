24c77b65a5e66c12fda6c0347a004a311b742ab2
dzchen314 <37160919+dzchen314@users.noreply.github.com>
2018-09-21 14:03:20 -0700

    Add model and frontend

--- application.py
+++ application.py
@@ -1,5 +1,4 @@
 import os
-import gensim
 import difflib
 from flask import Flask, jsonify, request, render_template, make_response, redirect, url_for, flash, session
 from flask_wtf import FlaskForm
@@ -7,15 +6,7 @@ from wtforms import StringField
 import logging
 import json
 import string
-
-def appid_to_name(gameid):
-    with open('appid_to_game.json','r') as file:
-        gamejson = json.load(file)
-    gamelist = gamejson['applist']['apps']
-
-    for ind, game in enumerate(gamelist):
-        if game['appid'] == gameid:
-            return game['name']
+from model import user_input_to_steamid, get_user_games, get_recs_from_appid, get_friends_recs, get_friends_games, get_user_recs, appid_to_name, is_multiplayer
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
@@ -32,18 +23,36 @@ def home():
 #Implement the model result here
 @app.route('/search',  methods=['GET', 'POST'])
 def recommendation():
-    user_input = request.args.get('steamid')
-    steamid = convert2steamid(user_input)
+    user_input = request.args.get('user_input')
+    steamid = user_input_to_steamid(user_input)
     user_games = get_user_games(steamid)
-    user_friends = get_user_friends(steamid)
-    friend_games = get_friend_games(steamid)
+    user_recs = get_user_recs(user_games)
+    friends_ids, friends_games = get_friends_games(steamid)
+    friends_recs = get_friends_recs(friends_ids, friends_games)
 
-    if steamid:
-        #sim = model.docvecs.most_similar(positive=difflib.get_close_matches(str(name), perfumeslist, 1), topn=10)
+    rec_intersect = {key:[] for key in friends_recs.keys()}
+    for i, friend_id in enumerate(friends_ids):
+        rec_intersect[friend_id] = set(friends_recs[friend_id]) & set(user_recs['appid'])
 
-        return render_template("recommendations.html")
+    if len(list(rec_intersect.keys())[0]) > 0:
+        common_friend1 = list(rec_intersect.keys())[0]
+        common_game1 = appid_to_name[list(rec_intersect[common_friend1])[0]]
 
-    return jsonify({'error' : 'Could not find your ID!'})
+    if len(list(rec_intersect.keys())[1]) > 0:
+        common_friend2 = list(rec_intersect.keys())[1]
+        common_game2 = appid_to_name[list(rec_intersect[common_friend2])[0]]
+
+    if len(list(rec_intersect.keys())[2]) > 0:
+        common_friend3 = list(rec_intersect.keys())[2]
+        common_game3 = appid_to_name[list(rec_intersect[common_friend3])[0]]
+
+    if len(list(rec_intersect.keys())[3]) > 0:
+        common_friend4 = list(rec_intersect.keys())[3]
+        common_game4 = appid_to_name[list(rec_intersect[common_friend4])[1]]
+
+    return render_template("recommendations.html", steamid = steamid, user_games = user_games, friends_recs = friends_recs, user_recs = user_recs, common_game1 = common_game1, common_friend1 = common_friend1, common_game2 = common_game2, common_friend2 = common_friend2, common_game3 = common_game3, common_friend3 = common_friend3, common_game4 = common_game4, common_friend4 = common_friend4)
+
+#    return jsonify({'error' : 'Could not find your ID!'})
 
 
 if __name__ == "__main__":
---
+++ model.py
@@ -0,0 +1,164 @@
+from urllib.request import Request, urlopen
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
+import requests
+import json
+import pickle
+import numpy as np
+import pandas as pd
+
+datapath = './static/data/'
+api_key = 'CC4C540D5F3EAD2DD6E9B4235E179455'
+
+BASE_URL = 'https://store.steampowered.com/app/'
+session = requests.Session()
+
+#import model
+with open(datapath + 'model_30.pickle', 'rb') as fle:
+    model = pickle.load(fle)
+
+#import dictionaries
+with open(datapath + 'appid_to_idx.txt', 'rb') as fle:
+    appid_to_idx = pickle.load(fle)
+
+with open(datapath + 'idx_to_appid.txt', 'rb') as fle:
+    idx_to_appid = pickle.load(fle)
+
+with open(datapath + 'steamid_to_idx.txt', 'rb') as fle:
+    steamid_to_idx = pickle.load(fle)
+
+with open(datapath + 'idx_to_steamid.txt', 'rb') as fle:
+    idx_to_steamid = pickle.load(fle)
+
+with open(datapath + 'appidx_to_name.txt', 'rb') as fle:
+    appidx_to_name = pickle.load(fle)
+
+with open(datapath + 'appid_to_name.txt', 'rb') as fle:
+    appid_to_name = pickle.load(fle)
+
+
+#get game similarity matrix
+def get_game_similarity(model):
+    game_similarity = model.item_embeddings.dot(model.item_embeddings.T)
+    #normalize by the max of each game-game embedding (i.e. self-embedding)
+    norm = np.array([np.sqrt(np.diagonal(game_similarity))])
+    game_similarity = game_similarity/norm/norm.T
+
+    return game_similarity
+
+game_similarity = get_game_similarity(model)
+
+#get the games most similar to a game in the user's library (ranked)
+def get_recs_from_appid(appid):
+    try:
+        appidx = appid_to_idx[appid]
+    except KeyError:
+        return []
+    recs = game_similarity[appidx]
+    df = pd.DataFrame({'game_idx':list(appidx_to_name), 'scores':recs})
+    df = df.sort_values(by='scores', ascending=False)
+    df['game_id'] = [idx_to_appid[idx] for idx in df.game_idx]
+    df['game_names'] = [appidx_to_name[idx] for idx in df.game_idx]
+
+    return df['game_id'].values, df['game_names'].values
+
+
+#make API call to get user games from steam ID
+def get_user_games(steamid):
+    req = Request('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=%s&steamid=%s&format=json&include_appinfo=True'%(api_key, steamid))
+
+    try:
+        data_raw = urlopen(req).read()
+        data_json = json.loads(data_raw)
+        user_games = data_json['response']['games']
+        return user_games
+    except:
+        return []
+
+def get_user_recs(user_games):
+    user_recs = {'appid':[]}
+    for i in range(0,34):
+        if i < 100:
+            try:
+                rec_id, rec_name = get_recs_from_appid(user_games[i]['appid'])
+                for i, r_id in enumerate(rec_id):
+                    if i < 5:
+                        user_recs['appid'].append(r_id)
+            except ValueError:
+                pass
+
+    return user_recs
+
+#make API call to get user friends from steam ID in the form of a dict {friendid:games}
+def get_friends_games(steamid):
+    req = Request('http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=%s&steamid=%s&relationship=friend'%(api_key, steamid))
+
+    try:
+        data_raw = urlopen(req).read()
+        data_json = json.loads(data_raw)
+        user_friends = data_json['friendslist']['friends']
+    except:
+        return []
+
+    friends_ids = [friend['steamid'] for friend in user_friends]
+    friends_games = {}
+    friends_games = {friend : get_user_games(friend) for friend in friends_ids}
+
+    return friends_ids, friends_games
+
+def get_friends_recs(friends_ids, friends_games):
+    eachfriends_recs = {key:[] for key in friends_ids}
+    for i, friend_id in enumerate(friends_ids):
+        friends_games_ids, friends_games_tplayed = sort_games_by_playtime(friends_games[friend_id])
+        for i, game_id in enumerate(friends_games_ids):
+            try:
+                rec_id, rec_name = get_recs_from_appid(game_id)
+                count = 0
+                for i, r_id in enumerate(rec_id):
+                    if count < 5:
+                        eachfriends_recs[friend_id].append(r_id)
+                        count += 1
+            except ValueError:
+                pass
+
+    return eachfriends_recs
+
+
+def sort_games_by_playtime(game_info):
+	game_ids = [game['appid'] for game in game_info]
+	playtimes = [game['playtime_forever'] for game in game_info]
+	userdf = pd.DataFrame({'appid': game_ids, 'hours_played' : playtimes})
+	userdf = userdf.sort_values(by='hours_played', ascending=False)
+
+	return userdf.appid.values, userdf.hours_played.values
+
+#check on the Steam website if the game is multiplayer
+def is_multiplayer(appid):
+    req = session.get(BASE_URL+str(appid))
+    soup = BeautifulSoup(req.content, 'html.parser')
+    soup = soup.find_all('a', {'class': 'app_tag'})
+    tagwords = []
+    for words in soup:
+        tagwords.append(words)
+
+    for i, words in enumerate(tagwords):
+        if str(words).find('Multiplayer') >= 0:
+            return True
+
+    return False
+
+#convert user input to steam ID
+def user_input_to_steamid(input_id):
+	req = Request('http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=%s&vanityurl=%s'%(api_key, input_id))
+
+	try:
+		data_raw = urlopen(req).read()
+	except HTTPError:
+		return input_id
+
+	data_json = json.loads(data_raw)
+
+	try:
+		return int(data_json['response']['steamid'])
+	except KeyError:
+		return input_id
