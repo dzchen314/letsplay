cc5e78e45145dca6d720236cfb4156ade36c237e
dzchen314 <37160919+dzchen314@users.noreply.github.com>
2018-09-25 09:36:00 -0700

    Updated front end, edited css

--- application.py
+++ application.py
@@ -1,12 +1,12 @@
 import os
 import difflib
-from flask import Flask, jsonify, request, render_template, make_response, redirect, url_for, flash, session
+from flask import Flask, jsonify, request, render_template, session
 from flask_wtf import FlaskForm
 from wtforms import StringField
 import logging
 import json
 import string
-from model import user_input_to_steamid, get_user_games, get_recs_from_appid, get_friends_recs, get_friends_games, get_user_recs, appid_to_name, is_multiplayer
+from model import user_input_to_steamid, get_user_games, get_recs_from_appid, get_friends_recs, get_friends_games, get_user_recs, appid_to_name, is_multiplayer, multiplayer_games
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
@@ -28,29 +28,18 @@ def recommendation():
     user_games = get_user_games(steamid)
     user_recs = get_user_recs(user_games)
     friends_ids, friends_games = get_friends_games(steamid)
-    friends_recs = get_friends_recs(friends_ids, friends_games)
+    friends_recs, allfriends_recs = get_friends_recs(friends_ids, friends_games)
+    friends_recs_games = [int(val) for val in allfriends_recs]
+    multigames = [int(val) for val in multiplayer_games]
 
-    rec_intersect = {key:[] for key in friends_recs.keys()}
+    rec_intersect = dict.fromkeys(friends_ids)
     for i, friend_id in enumerate(friends_ids):
-        rec_intersect[friend_id] = set(friends_recs[friend_id]) & set(user_recs['appid'])
+        rec_intersect[friend_id] = [int(id) for id in list(set(friends_recs[friend_id]) & set(user_recs['appid']))]
 
-    if len(list(rec_intersect.keys())[0]) > 0:
-        common_friend1 = list(rec_intersect.keys())[0]
-        common_game1 = appid_to_name[list(rec_intersect[common_friend1])[0]]
-
-    if len(list(rec_intersect.keys())[1]) > 0:
-        common_friend2 = list(rec_intersect.keys())[1]
-        common_game2 = appid_to_name[list(rec_intersect[common_friend2])[0]]
-
-    if len(list(rec_intersect.keys())[2]) > 0:
-        common_friend3 = list(rec_intersect.keys())[2]
-        common_game3 = appid_to_name[list(rec_intersect[common_friend3])[0]]
-
-    if len(list(rec_intersect.keys())[3]) > 0:
-        common_friend4 = list(rec_intersect.keys())[3]
-        common_game4 = appid_to_name[list(rec_intersect[common_friend4])[1]]
-
-    return render_template("recommendations.html", steamid = steamid, user_games = user_games, friends_recs = friends_recs, user_recs = user_recs, common_game1 = common_game1, common_friend1 = common_friend1, common_game2 = common_game2, common_friend2 = common_friend2, common_game3 = common_game3, common_friend3 = common_friend3, common_game4 = common_game4, common_friend4 = common_friend4)
+    return render_template("recommendations.html", steamid = steamid,
+    user_games = user_games, friends_recs_games = friends_recs_games,
+    user_recs = user_recs, friends_ids = [int(id) for id in friends_ids],
+    rec_intersect = rec_intersect, multigames = multigames)
 
 #    return jsonify({'error' : 'Could not find your ID!'})
 
